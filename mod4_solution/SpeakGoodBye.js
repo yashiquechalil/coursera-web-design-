f5cf307293bfa31aef5f574098e3430f1eae3de6
yashiquechalil <yashiquechalil@gmail.com>
2020-04-18 14:28:49 +0530

    final soln

---
+++ mod4_solution/SpeakGoodBye.js
@@ -0,0 +1,22 @@
+
+(function()
+{
+
+
+
+
+ var byeSpeaker = {};
+
+
+var speakWord = "Good Bye";
+
+
+byeSpeaker.speak = function(name) {
+  console.log(speakWord + " " + name);
+}
+
+
+
+window.byeSpeaker = byeSpeaker;
+
+})();
